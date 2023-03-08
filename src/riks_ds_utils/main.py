6d5aa0587e66d5870b339116c924f548dd05a133
Borg93 <48671678+Borg93@users.noreply.github.com>
2023-03-08 10:32:47 +0100

    Update main.py

--- src/riks_ds_utils/main.py
+++ src/riks_ds_utils/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def aggregate_mean(df:pd.Dataframe, column:str) -> dict:
+def aggregate_mean(df :pd.DataFrame, column:str) -> dict:
     """ Computes and returns the mean value of a column 
 
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -13,6 +13,17 @@ def aggregate_mean(df:pd.Dataframe, column:str) -> dict:
     
     Returns: 
        dict:  Mean value of each column as a dict.
+       
+    Usage:
+
+    How to use the function:
+
+    ```python
+    from riks_ds_utils.main import aggregate_mean
+
+    mean_dict = aggregate_mean(df, column)
+    
+    ```
     """
     return df.groupby("class")[column].mean().to_dict()
 
